18e4ebc7b4095de619c3ffb8ed770ab4629ba90b
Manuel Marcano <manuelmarcano22@yahoo.com>
2019-03-08 18:23:15 -0600

    Created a filter to then fixed astronemtry

--- misc/AstrometryMUSEwithHubble.py
+++ misc/AstrometryMUSEwithHubble.py
@@ -4,15 +4,20 @@
 
 
 from astropy.io import fits
-from mpdaf.obj import Image, WCS
+from mpdaf.obj import Image, WCS, Cube
 
 
 # In[11]:
 
+cube = Cube('../Observation1/data/CopyDATACUBE_FINAL.fits')
+imI = cube.get_band_image('Cousins_I')
+imI.write('Cousing_ITryFOV.fits')
+
 
 ### get offset between MUSE and HST ###
 imahst = Image('firstdraftwcsfixedicrshubblef606wimg.fits')
-cont = Image('CousingI.fits')
+#cont = Image('CousingI.fits')
+cont = Image('Cousing_ITryFOV.fits')
 ima2hst = cont.adjust_coordinates(imahst)
 
 ima2hst.write('FixedMUSEFOV.fits')
